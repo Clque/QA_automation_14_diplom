0857dfed4733216f0d2c8060f31caf502b873542
МуVerkhovskii <verkhovskii@etf-int.ru>
2024-03-24 20:50:42 +0300

    Add comments

--- test_create_user.py
+++ test_create_user.py
@@ -6,6 +6,5 @@ import data
 def test_status_code_200():
     create_order = send_request.post_new_order(data.body_order_with_optional)
     order_num = {"t": create_order.json()["track"]}
-    print(order_num)
     get_order_by_num = send_request.get_order_by_number(order_num)
-    assert get_order_by_num.status_code == 200
+    assert get_order_by_num.status_code == 200, "Статус код не равен 200"
