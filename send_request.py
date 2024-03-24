ca919f12e186e69206246a9fcf781020b7462963
МуVerkhovskii <verkhovskii@etf-int.ru>
2024-03-24 20:21:01 +0300

    Create request for /orders/track && /orders

---
+++ send_request.py
@@ -0,0 +1,9 @@
+import requests
+import configuration
+
+
+def post_new_order(order_body):
+    return requests.post(configuration.URL + configuration.CREATE_ORDER_PATH, json=order_body)
+
+def get_order_by_number(get_order_body):
+    return requests.get(configuration.URL + configuration.GET_ORDER_PATH, body=get_order_body)
