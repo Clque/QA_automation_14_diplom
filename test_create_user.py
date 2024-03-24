93e54f45f831bb4340bf854916598bd8ca86227c
МуVerkhovskii <verkhovskii@etf-int.ru>
2024-03-24 20:44:57 +0300

    Add comments

--- test_create_user.py
+++ test_create_user.py
@@ -1,7 +1,8 @@
+# Верховский Данила, 14-ая когорта - Финальный проект. Инженер по тестированию плюс.
 import send_request
 import data
 
-#
+
 def test_status_code_200():
     create_order = send_request.post_new_order(data.body_order_with_optional)
     order_num = {"t": create_order.json()["track"]}
