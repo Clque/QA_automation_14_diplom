f53bcc64c4fa4f2eaee93aebc574201d7f51999e
МуVerkhovskii <verkhovskii@etf-int.ru>
2024-03-24 20:14:19 +0300

    111

---
+++ data.py
@@ -0,0 +1,24 @@
+body_order_with_optional = {
+    "firstName": "Тест",
+    "lastName": "Тестов",
+    "address": "Улица Теста 5",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
+
+body_order_only_required = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha"
+}
